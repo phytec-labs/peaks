b0d8bf076cfba27614cc55414a075d1fb1b2bada
Nick McCarty <mccartyn@gmail.com>
2020-06-05 11:18:27 -0700

    tzdata package is still having an issue installing unattended. Looks like it failed last time because there was no /etc/localtime. Perhaps this is from changing the base docker image from ubuntu:latest to ubuntu:18.04

--- bsp-build-scripts/phycore-imx8m-mini/linux/PD-ALPHA2.sh
+++ bsp-build-scripts/phycore-imx8m-mini/linux/PD-ALPHA2.sh
@@ -30,6 +30,7 @@ echo "Installing host packages for build"
 
 #fix for tzdata package not allowing unattended installation...
 echo " Forcing timzeone to American EST to fix issue with tzdata package not allowing unattended install"
+mkdir /etc/localtime
 ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 
 export DEBIAN_FRONTEND=noninteractive
